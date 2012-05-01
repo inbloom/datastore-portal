eb935e6971c38d53035790104e87ca14659f2abc
Tapan Avasthi <tawasthi@wgen.net>
2012-05-01 15:30:33 +0530

    SCRIPT

--- installer/ProvisionScriptForTestServer.sh
+++ installer/ProvisionScriptForTestServer.sh
@@ -10,7 +10,7 @@ cd /opt/sli
 wget http://downloads.sourceforge.net/project/lportal/Liferay%20Portal/6.1.0%20GA1/liferay-portal-6.1.0-ce-ga1-20120106155615760.war
 mv liferay-portal-6.1.0-ce-ga1-20120106155615760.war portal.war
 
-sudo scp /opt/sli/portal.war tomcat@devlr1.slidev.org:/opt/test/webapps
+scp /opt/sli/portal.war tomcat@devlr1.slidev.org:/opt/test/webapps
 cd tomcat@devlr1.slidev.org:/opt/test/webapps/
 ls tomcat@devlr1.slidev.org:/opt/test/webapps
  
@@ -21,32 +21,32 @@ sh tomcat@devlr1.slidev.org:/opt/test/bin/catalina.sh stop
 
 
 #The setenv.sh has settings for the JVM that are used when tomcat starts
-sudo scp /jenkins/workspace/ProvisionScript/installer/conf/setenv.sh tomcat@devlr1.slidev.org:/opt/test/bin
-sudo chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/bin/setenv.sh
+  scp /jenkins/workspace/ProvisionScript/installer/conf/setenv.sh tomcat@devlr1.slidev.org:/opt/test/bin
+  chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/bin/setenv.sh
 
 # Adding jars that liferay depends on to lib/ext
-sudo mkdir -p tomcat@devlr1.slidev.org:/opt/test/lib/ext
-sudo chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/lib/ext
-sudo scp /jenkins/workspace/ProvisionScript/installer/conf/ext/*.jar tomcat@devlr1.slidev.org:/opt/test/lib/ext
-sudo chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/lib/ext
-sudo chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/lib/ext/*.jar
-sudo chmod 755 tomcat@devlr1.slidev.org:/opt/test/lib/ext
-sudo chmod 755 tomcat@devlr1.slidev.org:/opt/test/lib/ext/*.jar
+  mkdir -p tomcat@devlr1.slidev.org:/opt/test/lib/ext
+  chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/lib/ext
+  scp /jenkins/workspace/ProvisionScript/installer/conf/ext/*.jar tomcat@devlr1.slidev.org:/opt/test/lib/ext
+  chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/lib/ext
+  chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/lib/ext/*.jar
+  chmod 755 tomcat@devlr1.slidev.org:/opt/test/lib/ext
+  chmod 755 tomcat@devlr1.slidev.org:/opt/test/lib/ext/*.jar
 
 # Update catalina.properties to look for jars in the directory created above (lib/ext)
-sudo scp /jenkins/workspace/ProvisionScript/installer/conf/catalina.properties tomcat@devlr1.slidev.org:/opt/test/conf
-sudo chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/conf/catalina.properties
-sudo chmod 600 tomcat@devlr1.slidev.org:/opt/test/conf/catalina.properties
+  scp /jenkins/workspace/ProvisionScript/installer/conf/catalina.properties tomcat@devlr1.slidev.org:/opt/test/conf
+  chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/conf/catalina.properties
+  chmod 600 tomcat@devlr1.slidev.org:/opt/test/conf/catalina.properties
 
 # Copy the portal.xml file into tomcat 
 mkdir tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost
-sudo scp /jenkins/workspace/ProvisionScript/installer/conf/ROOT.xml tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost
-sudo chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost/portal.xml
-sudo chmod 600 tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost/portal.xml
+  scp /jenkins/workspace/ProvisionScript/installer/conf/ROOT.xml tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost
+  chown tomcat:tomcat tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost/portal.xml
+  chmod 600 tomcat@devlr1.slidev.org:/opt/test/conf/Catalina/localhost/portal.xml
 
 
 # Copy the portal-ext.properties file into tomcat
-sudo scp /jenkins/workspace/ProvisionScript/installer/conf/portal-ext.properties tomcat@devlr1.slidev.org:/opt/test/portal-ext.properties
+  scp /jenkins/workspace/ProvisionScript/installer/conf/portal-ext.properties tomcat@devlr1.slidev.org:/opt/test/portal-ext.properties
 
 sh tomcat@devlr1.slidev.org:/opt/test/bin/catalina.sh start
 tail -f tomcat@devlr1.slidev.org:/opt/test/logs/catalina.out
