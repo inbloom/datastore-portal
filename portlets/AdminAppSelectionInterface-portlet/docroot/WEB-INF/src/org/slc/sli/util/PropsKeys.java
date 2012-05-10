870033e4bb7af512a42e98d69680f4bdce22125a
Tapan Avasthi <tawasthi@wgen.net>
2012-05-10 21:02:45 +0530

    Rebranding Portlets

---
+++ portlets/AdminAppSelectionInterface-portlet/docroot/WEB-INF/src/org/slc/sli/util/PropsKeys.java
@@ -0,0 +1,9 @@
+package org.slc.sli.util;
+
+public class PropsKeys {
+	public static final String ROLE_IT_ADMINISTRATOR = "sli.role.itadmin";
+	
+	public static final String ROLE_SLI_ADMINISTRATOR = "sli.role.sliadmin";
+	
+	public static final String ROLE_EDUCATOR = "sli.role.educator";
+}
