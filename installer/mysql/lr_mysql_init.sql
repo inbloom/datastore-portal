8e29e8fbf66010ff0d86c73ed2a21ff2051215a8
Tapan Avasthi <tawasthi@wgen.net>
2012-05-12 19:47:53 +0530

    tapan Avasthi : OLD CODE INSTALLER

--- installer/mysql/lr_mysql_init.sql
+++
@@ -1,6 +0,0 @@
-CREATE Database lportal;
-use mysql;
-INSERT INTO user (Host,User,Password) VALUES('localhost','liferay',PASSWORD('liferaywgen'));
-flush privileges;
-grant all privileges on lportal.* to liferay@localhost;
-flush privileges;
