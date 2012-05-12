117aec1d5405a171be323ca6627128a256778048
Tapan Avasthi <tawasthi@wgen.net>
2012-05-12 19:38:43 +0530

    tapan Avasthi

--- hooks/deploylar-hook/docroot/WEB-INF/src/org/slc/sli/util/PropsKeys.java
+++
@@ -1,9 +0,0 @@
-package org.slc.sli.util;
-
-public class PropsKeys {
-
-	public static final String LAYOUT_LAR_FILE_NAME = "layout.lar.file.name";
-	
-	public static final String AUTO_DEPLOY_DEPLOY_DIR = "auto.deploy.deploy.dir";
-	
-}
