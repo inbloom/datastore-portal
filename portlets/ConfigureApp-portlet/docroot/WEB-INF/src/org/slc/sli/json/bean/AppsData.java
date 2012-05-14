7c0012c5857cddcd1eeb9f9e2b193883e3d46941
mmali <mmali@wgen.net>
2012-05-14 15:10:14 +0530

    conf app portlet added

---
+++ portlets/ConfigureApp-portlet/docroot/WEB-INF/src/org/slc/sli/json/bean/AppsData.java
@@ -0,0 +1,68 @@
+package org.slc.sli.json.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class AppsData implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+		private String name;
+		private String description;
+		private String behaviour;
+		private String image_url;
+		private String application_url;
+		private String admin_url;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getBehaviour() {
+			return behaviour;
+		}
+
+		public void setBehaviour(String behaviour) {
+			this.behaviour = behaviour;
+		}
+
+		public String getImage_url() {
+			return image_url;
+		}
+
+		public void setImage_url(String image_url) {
+			this.image_url = image_url;
+		}
+
+		public String getApplication_url() {
+			return application_url;
+		}
+
+		public void setApplication_url(String application_url) {
+			this.application_url = application_url;
+		}
+
+		public String getAdmin_url() {
+			return admin_url;
+		}
+
+		public void setAdmin_url(String admin_url) {
+			this.admin_url = admin_url;
+		}
+
+	}
