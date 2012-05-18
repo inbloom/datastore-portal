5cebbb47063d409490a7a3a3901cbc9c907a8589
mmali <mmali@wgen.net>
2012-05-18 12:03:22 +0530

    DE446- Added header/footer portlet

---
+++ portlets/headerfooter-portlet/docroot/WEB-INF/src/org/slc/sli/util/Constants.java
@@ -0,0 +1,19 @@
+package org.slc.sli.util;
+
+/**
+ * This class is for constants that are used in multiple places throughout the
+ * application. Constants used only in one class should be kept in that class.
+ * 
+ * @author
+ * 
+ */
+public class Constants {
+
+	public static final String HTTP_PREFIX = "https://";
+
+	// API related URLs
+	public static final String API_PREFIX = "api/rest";
+
+	public static final String SESSION_CHECK_PREFIX = "api/rest/system/session/check";
+
+}
