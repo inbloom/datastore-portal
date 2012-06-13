f9463cfb446daafbd6fa565e76f7252445b15323
Chad Wilson <cwilson@wgen.net>
2012-06-12 21:16:55 -0400

    update hostname as not registering in build

--- profile_swap.sh
+++ profile_swap.sh
@@ -7,5 +7,5 @@ else
 fi
 
 #Update environment.properties
-sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$Hostname.slidev.org/g" team.properties 
+sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$hostname.slidev.org/g" team.properties 
 echo "Done.. ready to build and deploy!"
