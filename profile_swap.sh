ecb3b60d045611291939530af8fcc30362cf4f21
Chad Wilson <cwilson@wgen.net>
2012-06-12 21:08:41 -0400

    Re-pushing "Add profile_wap file for Portal to populate environment.properties

--- profile_swap.sh
+++ profile_swap.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/bash
 
-#Take care of build profiles for Spring
-echo "Altering dashboard/api/ingetion projects..."
-grep -lR "sli.dev.subdomain=" sli-configuration/* | xargs -L 1 sed -i'' -e "s/sli\.dev\.subdomain=.*/sli.dev.subdomain=$1/g" 
+if [ $# -eq 1 ]; then
+    hostname=$1
+else
+    hostname=`hostname -s`
+fi
 
-#Take care of fixture data for applications
-echo "Altering fixture data for applications to match..."
-sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/g" acceptance-tests/test/data/team_application_fixtures.json
-
-#Take care of rails projects
-echo "Altering rails applications to match..."
-grep -lR "https://ci.slidev.org" admin-tools/admin-rails/config/config.yml | xargs -L 1 sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/g" 
-grep -lR "https://ci.slidev.org" databrowser/config/config.yml | xargs -L 1 sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/g" 
-sed -i'' -e "s/ci.slidev.org/$1.slidev.org/g" admin-tools/admin-rails/config/deploy/team.rb 
-sed -i'' -e "s/ci.slidev.org/$1.slidev.org/g" databrowser/config/deploy/team.rb 
-
+#Update environment.properties
+sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$Hostname.slidev.org/g" team.properties 
 echo "Done.. ready to build and deploy!"
