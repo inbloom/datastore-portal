cb5ef5035da1adfb59e88f3276b0e11a899bceaa
mmali <mmali@wgen.net>
2012-06-29 21:14:15 +0530

    DE1060

--- webs/sso-web/docroot/WEB-INF/src/org/slc/sli/login/servlet/filter/sso/SLIFilter.java
+++ webs/sso-web/docroot/WEB-INF/src/org/slc/sli/login/servlet/filter/sso/SLIFilter.java
@@ -88,7 +88,7 @@ public class SLIFilter extends BasePortalFilter {
 			if (_log.isDebugEnabled()) {
 				_log.debug("Logout called");
 			}
-			if (client != null) {
+			if (client != null && token != null) {
 				_log.info("client not null");
 				SLISSOUtil.logout(client, request, response);
 			}else{
