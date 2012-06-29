60edc488081bc1d0004e08f3e82c94b3c15083da
mmali <mmali@wgen.net>
2012-06-29 14:05:00 +0530

    DE1060 validator error resolved

--- webs/sso-web/docroot/WEB-INF/src/org/slc/sli/login/servlet/filter/sso/SLIFilter.java
+++ webs/sso-web/docroot/WEB-INF/src/org/slc/sli/login/servlet/filter/sso/SLIFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.slc.sli.util.CookieKeys;
+import com.liferay.portal.kernel.util.Validator;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
