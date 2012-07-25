4a6cdc37ecb53155ed3f805cd05136868f81cbfa
Takashi Osako <tosako@wgen.net>
2012-07-25 11:06:21 -0400

    portal installation script

--- installer/portal-local-install.sh
+++ installer/portal-local-install.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-DELETE_OPT=0
+EXECUTE_ALL=0
+PURGE_OPT=0
 INTERACTIVE=0
+DATABASE_INIT=0
+UPDATE_JSON=0
 
-while getopts id o
+while getopts aipdj o
 do
    case  "$o" in
+      a)   EXECUTE_ALL=1;;
       i)   INTERACTIVE=1;;
-      d)   DELETE_OPT=1;;
-      [?])   echo "-i (interactive mode)"
-             echo "-d (delete /opt before install)"
+      p)   PURGE_OPT=1;;
+      d)   DATABASE_INIT=1;;
+      j)   UPDATE_JSON=1;;
+      [?])   echo "-a (equivalent of -p -d -j)"
+             echo "-i (interactive mode to setup Tomcat and Portal)"
+             echo "-p (purge /opt before install)"
+             echo "-d (drop and create loption database)"
+             echo "-j (update JSON file then update mongo)"
+             echo "useful link https://thesli.onconfluence.com/display/sli/Checkout+and+Build+Portal"
            exit 1;;
    esac
 done
+
+if [ ${EXECUTE_ALL} == 1 ]; then
+   PURGE_OPT=1
+   DATABASE_INIT=1
+   UPDATE_JSON=1
+fi
 
 WGET=`which wget`
 
@@ -49,10 +65,6 @@ CLIENT_ID="lY83c5HmTPX"
 CLIENT_SECRET="ghjZfyAXi7qwejklcxziuohiueqjknfdsip9cxzhiu13mnsX"
 API="http://local.slidev.org:8080/"
 
-if [ ${DELETE_OPT} == 1 ]; then
-   sudo rm -rf ${OPT}
-fi
-
 if [ ${INTERACTIVE} == 1 ]; then
    PID=$$
 
@@ -80,6 +92,29 @@ if [ ${INTERACTIVE} == 1 ]; then
    API=`cat /tmp/portal-install.${PID}`
    rm -f /tmp/portal-install.${PID}
 fi
+
+if [ ${DATABASE_INIT} == 1 ]; then
+   MYSQL=`which mysql`
+   if [ -z "${MYSQL}" ]; then
+      echo "mysql command not found in PATH, or it may not be installed"
+      echo "Abort installation"
+      exit 1
+   fi
+   echo "Dropping lportal database"
+   mysqladmin drop lportal -u root
+   mysql -u root < ${LIFERAY_HOME}/installer/mysql/lr_mysql_init.sql
+fi
+
+if [ ${PURGE_OPT} == 1 ]; then
+   dialog --title "Deleting ${OPT} directory"  --backtitle "Portal Local Install"  --yesno "Are you sure you want to permanetly delete \"${OPT}\"?" 8 80
+   YES_NO=$?
+   case $YES_NO in
+      0) sudo rm -rf ${OPT};;
+      1) echo "${OPT} directory not deleted";;
+      255) echo "[ESC] key pressed. I am exiting out"; exit;;
+   esac
+fi
+
 
 
 
@@ -192,11 +227,31 @@ if [ ${RM_PORTAL} == 1 ]; then
 fi
 mv /opt/portal/environment.properties ${OPT}/
 rm -rf /opt/portal/
+if [ ${UPDATE_JSON} == 1 ]; then
+   echo "UPDATING JSON"
+   grep 213ee853-8983-fe48-bf5e-fde3c3a6437b ${SLI_HOME}/acceptance-tests/test/data/application_fixture.json > /dev/null 2>&1
+   JSON_DATA=$?
+   if [ ${JSON_DATA} == 1 ]; then
+      echo '{ "_id" : "213ee853-8983-fe48-bf5e-fde3c3a6437b", "type" : "application", "body" : { "authorized_ed_orgs" : ["IL-SUNSET", "IL", "IL-DAYBREAK", "15", "NC-KRYPTON", "GALACTICA", "CAPRICA", "PICON", "SAGITTARON", "VIRGON", "NY-Parker","NY-Dusk", "NY", "SC-OVERLORD", "KS-GREATVILLE", "KS-SMALLVILLE","KS-OVERLORD"], "version": "0.0", "image_url": "http://placekitten.com/150/150", "administration_url": "http://local.slidev.org:7000/c/portal/login", "application_url":"http://local.slidev.org:7000/c/portal/login", "client_secret" : "ghjZfyAXi7qwejklcxziuohiueqjknfdsip9cxzhiu13mnsX", "registration": {"status": "APPROVED", "request_date": 1330521193111, "approval_date": 1330521193111}, "redirect_uri" : "http://local.slidev.org:7000/portal/c/portal/login", "description" : "Portal Local", "name" : "Portal Local", "is_admin": false, "created_by": "slcdeveloper", "installed": false, "client_id" : "lY83c5HmTPX", "behavior": "Full Window App", "vendor" : "SLC"}, "metaData" : { "updated" : { "$date" : 1330521193111 }, "created" : { "$date" : 1330521193111 }} }' >> ${SLI_HOME}/acceptance-tests/test/data/application_fixture.json
+      echo '{ "_id" :{ "$binary" : "D0j+8nc6kW62GFLgZnwNnA==", "$type" : "03" }, "type" : "applicationAuthorization", "body" :{ "authId" : "IL-DAYBREAK", "authType" : "EDUCATION_ORGANIZATION", "appIds" : [ "78f71c9a-8e37-0f86-8560-7783379d96f7", "deb9a9d2-771d-40a1-bb9c-7f93b44e51df", "148eebc7-e320-4e35-b7c2-238e90dbd957", "1ad39ff1-65f8-4a16-8912-b49872f1ee96", "63e006bd-fc5f-450f-89c2-69bdfa979c43", "ee3e4e95-0c28-4110-b41b-b29cdec344e6", "25d21fdd-7e97-4aa4-aed0-6d6592a35bb2", "7a8a28cf-f9f2-4ea4-a238-d11b84a3dad2", "8a3def8c-016c-454a-a3ef-cf851e386d4e", "df91814a-ae2e-47f9-b642-742b8c26d65f", "25d21fdd-7e97-4aa4-79d0-6d6592a35bb2", "7a8a28cf-f9f2-4ea4-7938-d11b84a3dad2", "df91814a-ae2e-47f9-7942-742b8c26d65f", "7ae2a2f6-09dc-455d-b139-4d0da6189b52", "af264911-f638-4ec7-b792-69a2a2949b1e", "71ee2a92-788a-4dff-9d72-f1b6f0670aa7", "f4ad653b-2108-4e92-864c-e03722e5ef82", "b26e9c15-54d5-43d5-9d6f-52b8998a6047", "6387e94d-1912-4918-85e0-4f38014253cf", "213ee853-8983-4e40-bf5e-fde3c3a6437b", "52ab3769-1655-4542-971c-1fa02b1b368d", "1ef3990c-e0b5-4046-985c-fbcbab680bb9", "19cca28d-7357-4044-8df9-caad4b1c8ee4", "ee3e4e95-0c28-5114-b41b-b29cdec344e6", "2274ab86-6e21-ab75-9d6f-52b8998a6007", "eab130ba-6e5a-ab75-864c-e03722e523c2", "c09628da-9ea2-d969-85e0-4f3801425388", "c07a656a-6a2c-d9b1-85e0-4fe419425b3b", "1abf3940-84cc-11e1-b0c4-0800200c9a66", "c6251365-e571-482e-8f80-aaece6fd5136", "206e28d3-89a9-db4a-8f80-aaece6fda529", "213ee853-8983-fe48-bf5e-fde3c3a6437b" ] }, "metaData" : { "updated" :{ "$date" : 1332785105123 }, "created" :{ "$date" : 1332785105123 }} }' >> ${SLI_HOME}/acceptance-tests/test/data/application_fixture.json
+   fi
+   echo
+   echo
+   echo
+   echo "######## REQURED ########"
+   echo "Please run bundle exec rake realmInitNoPeople"
+   echo
+   echo
+   echo
+fi
 echo "########## Next Step ###########"
 echo "setup Tomcat for Eclipse"
 echo "Append following line to Eclipse Tomcat 'General Information'->'Open launch configuration'->'Arguments' tab->'VM arguments:'"
 echo "Please make sure you select 'Use custom location (does not modify Tomcat installation)' and set '/opt/tomcat' for 'Server path:' and set 'webapps' for 'Deploy path:' before saving it"
 echo "https://thesli.onconfluence.com/display/sli/Checkout+and+Build+Portal and refer 'Option 2. Script Driven Install'"
+if [ ${DATABASE_INIT} == 1 ]; then
+   echo "Your lportal database has been newly created.  This is very important that Tomcat server needs to start \"TWICE\" before you login to portal"
+fi
 echo "############# BEGIN ###########"
 echo "-Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Xmx1024m -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -XX:MaxPermSize=512m -Dsli.encryption.keyStore=${ENCRYPTION_DIR}/ciKeyStore.jks -Dsli.encryption.properties=${ENCRYPTION_DIR}/ciEncryption.properties -Dsli.trust.certificates=${ENCRYPTION_DIR}/trustedCertificates -Dsli.conf=${PORTAL_TOMCAT}/sli.properties"
 echo "############# END #############"
